6b5852147caa6c90d5a8fcf3d3fa561ab6cceef7
zendepatil <sanketzende95@gmail.com>
2020-08-04 23:37:45 +0530

    deploy site

---
+++ precache-manifest.8397f24c2fc2e510acd7392fbc36e845.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e0220e308999fcf4cf32680d378206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8810aa6f584521f594",
+    "url": "/static/css/2.490f1af6.chunk.css"
+  },
+  {
+    "revision": "4cfe2eec1796f7de89ee",
+    "url": "/static/css/main.bf706735.chunk.css"
+  },
+  {
+    "revision": "af8810aa6f584521f594",
+    "url": "/static/js/2.45d9e450.chunk.js"
+  },
+  {
+    "revision": "4cfe2eec1796f7de89ee",
+    "url": "/static/js/main.77b277f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
